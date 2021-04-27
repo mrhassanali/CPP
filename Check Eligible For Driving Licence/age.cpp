458c997516dbcb51f5b935f886967bb75f412a67
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2021-04-27 19:31:01 +0500

    Add files via upload

---
+++ Check Eligible For Driving Licence/age.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter the Age=";
+    cin>>age;
+    if(age>=18)
+    {
+        cout<<"Your are Eligible for driving Licence";
+
+    }
+    else
+    {
+        cout<<"You are not eligible for driving Licence";
+    }
+    getch();
+    
+}
